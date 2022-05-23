a87de627b11e637fe859d74db9fc8e45b7bc1578
Adar Dagan <AdarDagan@outlook.com>
2022-05-23 11:15:47 +0300

    add deploy and test fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'The-Site'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('The-Site');
+    expect(app.title).toEqual('Th-Site');
   });
 
   it('should render title', () => {
