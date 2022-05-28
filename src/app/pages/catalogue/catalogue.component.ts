08b93b70625d8606036ebc812b09cd7600ad1a30
Adar-Dagan <101581112+Adar-Dagan@users.noreply.github.com>
2022-05-28 10:19:48 +0300

    Add skeleton of site (#11)

---
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogue',
+  templateUrl: './catalogue.component.html',
+  styleUrls: ['./catalogue.component.less']
+})
+export class CatalogueComponent { }
